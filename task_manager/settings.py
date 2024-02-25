966e15da1f40dd468f6f4b46afb779b2de30bf84
Alexander Shangin <alarriva@yandex.ru>
2024-02-25 15:17:40 +0600

    Refactor settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -15,8 +15,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,15 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# 'django-insecure-^zt!vih!4i$_!7skf8ocooc@fk#4a9cw3h%c)pjf@b%ina$o!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 
+print(ALLOWED_HOSTS)
 
 # Application definition
 
@@ -100,6 +99,8 @@ DATABASES = {
 #         )
 #     }
 database_url = os.environ.get("DATABASE_URL")
+print(database_url)
+
 # DATABASES["default"] = dj_database_url.parse("postgres://task_manager_un3g_user:as8xbW7nn8UZCVEl11mWlXA2UeT9Oih5@dpg-cnd235qcn0vc73f4orpg-a.frankfurt-postgres.render.com/task_manager_un3g")
 
 
@@ -145,24 +146,18 @@ USE_TZ = True
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 
-# Tell Django to copy statics to the `staticfiles` directory
-# in your application directory on Render.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Ниже до дефолта мои старые статики
-# STATIC_URL = '/static/'
-# # This production code might break development mode, so we check whether we're in DEBUG mode
-# if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
