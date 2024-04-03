fe19c73943087bd1bd48ddfef8c36cdb32a25f24
Alexander Shangin <alarriva@yandex.ru>
2024-04-03 11:34:32 +0600

    Fix files

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from task_manager.users import views
 
 urlpatterns = [
-    path('', views.IndexUser.as_view()),
+    path('', views.UserList.as_view(), name='user_list'),
+    # path('<int:user_id>/edit/', edit_user, name='edit_user'),
+    # path('<int:user_id>/delete/', delete_user, name='delete_user'),
+    
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from .models import CustomUser
 # from django.utils.translation import gettext as _
 
 
-class IndexUser(View):
+# class IndexUser(View):
 
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'users/user_list.html')
+
+class UserList(View):
+    
     def get(self, request, *args, **kwargs):
-        return render(request, 'users/index.html')
+        users = CustomUser.objects.all()
+        return render(request, 'users/user_list.html', {'users': users})
